a5376cd52c2c8be2dddd48f1912ed73c39127b3a
Michael Hartl <michaelhartl000@gmail.com>
2021-09-08 13:03:35 +0200

    update functions and test to reflect folder structure assumed in exercise.
    The working directory needs to be set to the folder contiaining 'specdata'
    in order for the scripts to run.

--- week2/functions.R
+++ week2/functions.R
@@ -4,7 +4,7 @@
 
 load.file <- function(directory, i) {
     file.int <- sprintf("%03i", i)
-    file.name <- paste(directory, file.int, ".csv", sep="")
+    file.name <- paste(directory, "/", file.int, ".csv", sep="")
     data <- read.csv(file.name)
     data
 }
--- week2/test.R
+++ week2/test.R
@@ -1,21 +1,21 @@
-source("documents/R-Coursera-Course/week2/functions.R")
+source("~/documents/R-Coursera-Course/week2/functions.R")
 
-head(load.file(directory = "documents/R-Coursera-Course/data/specdata/", i=2))
+head(load.file(directory = "specdata", i=2))
 
-head(load.files(directory = "documents/R-Coursera-Course/data/specdata/", id=2:3))
+head(load.files(directory = "specdata", id=2:3))
 
 pollutantmean(
-    directory = "documents/R-Coursera-Course/data/specdata/",
+    directory = "specdata",
     pollutant = "nitrate",
     id = 1:5
 )
 
 complete(
-    directory = "documents/R-Coursera-Course/data/specdata/", 
+    directory = "specdata", 
     id=c(2, 4, 8, 10, 12)
 )
 
-corr("documents/R-Coursera-Course/data/specdata/", 400)
+corr("specdata", 400)
 
 
 
